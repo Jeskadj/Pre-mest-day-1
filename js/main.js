0e4496cc2d1f70f4dc9e60f59be05dff8705997d
jeskadj <adjeteyj@ymail.com>
2021-03-04 11:45:59 +0000

    book

--- js/main.js
+++ js/main.js
@@ -1,7 +1,23 @@
 console.log("hellooooo.......")
-
+//variables
 let b=78
 
 console.log(b);
 
+let g= "afia"
 
+console.log (g)
+
+let a=5
+let c=9
+console.log(a+c)
+console.log(a*c)
+
+let surname ="mensah"
+let firstname="joseph"
+console.log(firstname+surname)
+
+let middlename="lois"
+let lastname="agyei"
+
+console.log("my,name,is", middlename+ ' '+lastname)
